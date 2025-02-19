2260021373f0a0eb6cebafc3c7de6ded026b0786
Alex Guerra <alexander.a.guerra@gmail.com>
2025-02-19 07:39:27 -0600

    Mass commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 'use client'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { createDevice } from '@rnbo/js'
 
 import DrumMachine from '../components/DrumMachine'
 
+let WAContext = window.AudioContext || (window as any).webkitAudioContext
+let context = new WAContext()
 export default function Home() {
     useEffect(() => {
-        let WAContext =
-            window.AudioContext || (window as any).webkitAudioContext
-        let context = new WAContext()
-
         const setup = async () => {
             let rawPatcher = await fetch('/export/patch.export.json')
             let patcher = await rawPatcher.json()
@@ -24,12 +22,36 @@ export default function Home() {
         setup()
     }, [])
 
+    const start = () => {
+        console.log(`The context is ${context.state}`)
+        console.log('Going to start the context')
+        context.resume()
+        console.log(`The context is now ${context.state}`)
+    }
+
+    const stop = () => {
+        console.log(`The context is ${context.state}`)
+        console.log('Going to stop the context')
+        context.suspend()
+        console.log(`The context is now ${context.state}`)
+    }
+
     return (
         <div className="bg-red-300 text-8xl">
             <h1>First Page</h1>
             <p>Gunna be a synth thing</p>
+
             <div className="bg-green-600 w-20 h-20">hi</div>
             <DrumMachine />
+            <div className="flex flex-col p-100 m-20">
+                <button className="bg-green-700" onClick={start}>
+                    Click Me To Start
+                </button>
+                <button className="bg-red-800" onClick={stop}>
+                    Click Me To Stop
+                </button>
+                <button>Click Me To List All The Paramiters</button>
+            </div>
         </div>
     )
 }
